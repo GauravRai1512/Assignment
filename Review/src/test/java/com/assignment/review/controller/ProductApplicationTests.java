ae66669b26ec34b545e6a7764d250f4da8771f32
Gaurav Rai <gkraisonurai@gmail.com>
2023-04-19 13:19:31 +0530

    test cases added

--- Review/src/test/java/com/assignment/review/controller/ProductApplicationTests.java
+++ Review/src/test/java/com/assignment/review/controller/ProductApplicationTests.java
@@ -1,8 +1,12 @@
 package com.assignment.review.controller;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.assignment.review.model.ProductReviewRequest;
+import com.assignment.review.model.RetrieveProductReviewResponse;
 import com.assignment.review.services.ReviewService;
 
 
@@ -35,6 +40,38 @@ class ProductReviewControllerTest {
 		
 		ResponseEntity<String> responseSuccess = productReviewController.createProductReview(productReview);
 		assertEquals(responseSuccess.getStatusCode(), HttpStatus.CREATED);
+		
+	}
+	
+	@Test
+	public void testGetProductReview() {
+		RetrieveProductReviewResponse responseSuccess = productReviewController.getProductReview("AC7836");
+		assertNotNull(responseSuccess);
+		
+	}
+	
+	@Test
+	public void testGetProductReviewNotFound() {
+		RetrieveProductReviewResponse responseSuccess = productReviewController.getProductReview("AC783656");
+		assertNull(responseSuccess, "response is null");
+		
+	}
+	
+	@Test
+	public void testGetAllProductReview() {
+		List<RetrieveProductReviewResponse> responseSuccess = productReviewController.getAllProductReview();
+		assertNotNull(responseSuccess);
+		
+	}
+	
+	@Test
+	public void testPutProductReview() {
+		ProductReviewRequest productReview = new ProductReviewRequest();
+		productReview.setProductID("AC783656");
+		productReview.setAverageReviewScore(new BigDecimal(5.4));
+		productReview.setNumerofReviews(14);
+		RetrieveProductReviewResponse responseSuccess = productReviewController.updatedProductReview("AC783656", productReview);
+		assertNotNull(responseSuccess);
 		
 	}
 
